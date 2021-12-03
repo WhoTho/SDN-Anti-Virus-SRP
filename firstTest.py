c67264a82a164f5ad6d9753a1b1e6e8f4fd0de51
WhoTho <77036198+WhoTho@users.noreply.github.com>
2021-12-02 23:35:49 -0500

    topo test

--- firstTest.py
+++ firstTest.py
@@ -1,1 +1,14 @@
-print("Hello World")
+from mininet.topo import Topo
+
+class TestTopo(Topo):
+    def __init__(self):
+        Topo.__init__(self)
+
+        leftHost = self.addHost('h1')
+        rightHost = self.addHost('h2')
+        leftSwitch = self.addSwitch('s1')
+
+        self.addLink(leftHost,leftSwitch)
+        self.addLink(rightHost,leftSwitch)
+
+topos = { 'mytopo': {lambda: TestTopo()}}
