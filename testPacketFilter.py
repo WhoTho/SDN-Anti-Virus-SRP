63ad0ff856d22cd09691f7d32f78e2fd1eae1ccb
WhoTho <77036198+WhoTho@users.noreply.github.com>
2021-12-03 15:33:22 -0500

    a

---
+++ testPacketFilter.py
@@ -0,0 +1,33 @@
+#https://ryu.readthedocs.io/en/latest/writing_ryu_app.html#start-writing
+
+
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_0
+
+class L2Switch(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(L2Switch, self).__init__(*args, **kwargs)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER) # MAIN_DISPATCHER as the second argument means this function is called only after the negotiation completes
+    def packet_in_handler(self, ev):
+        msg = ev.msg
+        print(f"Packet in >>> {msg}")
+        dp = msg.datapath
+        ofp = dp.ofproto
+        ofp_parser = dp.ofproto_parser
+
+        actions = [ofp_parser.OFPActionOutput(ofp.OFPP_FLOOD)]
+
+        data = None
+        if msg.buffer_id == ofp.OFP_NO_BUFFER:
+             data = msg.data
+
+        out = ofp_parser.OFPPacketOut(
+            datapath=dp, buffer_id=msg.buffer_id, in_port=msg.in_port,
+            actions=actions, data = data)
+        dp.send_msg(out)
