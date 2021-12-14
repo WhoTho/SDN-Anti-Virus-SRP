f613165e50603b8cae7625e943cd4e1c2d9d1a02
WhoTho <77036198+WhoTho@users.noreply.github.com>
2021-12-13 23:19:43 -0500

    testing

--- testML.py
+++ testML.py
@@ -1,55 +1,44 @@
-import numpy as np
-import tensorflow as tf
-from tensorflow import keras
+import itertools as IT
 
-print(tf.version)
+import numpy as NP
+import tensorflow as TF
+from tensorflow import keras as KR
 
-# gpu_devices = tf.config.experimental.list_physical_devices('GPU')
-# for device in gpu_devices:
-#     tf.config.experimental.set_memory_growth(device, True)
+# Simple code to generate sample test data
+perms = set(list(IT.permutations([0]*4+[1]*4,4)))
+inputTemp = []
+outputTemp = []
+print(perms)
+for p in perms:
+    inputTemp.append(p)
+    a = p[0]^p[1]
+    b = p[2]^p[3]
+    outputTemp.append([a,b])
 
-# import tensorflow_text as tf_text
+print(inputTemp)
+print(outputTemp)
+inputDataTrain = NP.array(inputTemp)
+outputDataTrain = NP.array(outputTemp)
 
-# def preprocess(vocab_table, example_text):
-
-#   # Normalize text
-#   tf_text.normalize_utf8(example_text)
-
-#   # Tokenize into words
-#   word_tokenizer = tf_text.WhitespaceTokenizer()
-#   tokens = word_tokenizer.tokenize(example_text)
-
-#   # Tokenize into subwords
-#   subword_tokenizer = tf_text.WordpieceTokenizer(
-#        lookup_table, token_out_type=tf.int64)
-#   subtokens = subword_tokenizer.tokenize(tokens).merge_dims(1, -1)
-
-#   # Apply padding
-#   padded_inputs = tf_text.pad_model_inputs(subtokens, max_seq_length=16)
-#   return padded_inputs
-
-
-inputDataTrain = np.array([[0,0],[0,1],[1,0],[1,1]])
-outputDataTrain = np.array([[0],[1],[1],[0]])
-inputDataValidate = np.array([[0,0]])
-
-model = keras.Sequential([
-    keras.layers.Dense(4, input_dim=2, activation='relu'),
-    keras.layers.Dense(1, activation='sigmoid')
+model = KR.Sequential([
+    KR.layers.Dense(8, input_dim=4, activation='relu'),
+    KR.layers.Dense(2, activation='sigmoid')
 ])
 
 
 model.compile(optimizer='adam',
               loss='mean_squared_error',
-              metrics=['accuracy'])
+              metrics='accuracy')
 
 
 history = model.fit(
     inputDataTrain,
     outputDataTrain,
-    epochs=500,
-    #verbose=0
+    epochs=10000,
+    verbose=0
 )
 
-predictions = model.predict(inputDataValidate).round()
+predictions = model.predict(inputDataTrain).round()
 print(predictions)
+evaluation = model.evaluate(inputDataTrain,outputDataTrain)
+print(evaluation)
