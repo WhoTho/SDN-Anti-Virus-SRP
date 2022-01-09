54588d407c168b3e88d6913870e6423b1bb600dd
WhoTho <77036198+WhoTho@users.noreply.github.com>
2022-01-09 17:03:45 -0500

    changed model type to xgbclassifier

--- XGBoostModel.py
+++ XGBoostModel.py
@@ -1,64 +1,43 @@
 import matplotlib.pyplot as MPL
 import numpy as NP
 import sklearn.metrics as SKL
-import xgboost as XGB
 from sklearn import model_selection as SKLMS
-from xgboost import DMatrix, XGBClassifier
+from xgboost import XGBClassifier
 
 filePath = lambda name:"/home/gopal/Desktop/srpantivirus/packetDatasets/"+name
-
-
-
-# maliciousInput = NP.loadtxt(filePath("malicious"), delimiter="\t")
-# maliciousOutput = NP.ones(len(maliciousInput))
-# benignInput = NP.loadtxt(filePath("benign"), delimiter="\t")
-# benignOutput = NP.zeros(len(benignInput))
 
 fullInput = NP.loadtxt(filePath("mixedPacketInfo"), delimiter="\t")
 fullOutput = NP.loadtxt(filePath("mixedPacketType"), delimiter="\t")
 Xtrain,Xtest,Ytrain,Ytest = SKLMS.train_test_split(fullInput,fullOutput,stratify=fullOutput,random_state=10)
 assert(len(fullInput)==len(fullOutput))
 
-labels = ["Duration","Size of flows orig","total size of flows resp","ratio of sizes","outbound packets","inbound packets","length of certificate path","certificate length","number of domains in certificate"]
+labels = ["Duration","Size of flows orig","total size of flows resp","ratio of sizes","outbound packets","inbound packets","length of certificate path","number of domains in certificate","certificate length"]
 
-dataTrain = DMatrix(Xtrain,label=Ytrain,feature_names=labels)
-dataTest = DMatrix(Xtest,label=Ytest,feature_names=labels)
-
-
-param = {"verbosity":0,"max_depth":9, "eta":0.01,"objective":"binary:hinge", "eval_metric":"error"}
 numberOfRounds = 1000
+watchlist = [(Xtrain,Ytrain),(Xtest,Ytest)]
+param = {"use_label_encoder":False,"verbosity":0,"max_depth":9,"eta":0.01,"objective":"binary:hinge","n_estimators":numberOfRounds,"eval_set":watchlist,"early_stopping_rounds":100}
 
-watchlist = [(dataTrain,'train'),(dataTest,'eval')]
-evals_result = {}
-model = XGB.train(param, dataTrain, numberOfRounds, watchlist, evals_result=evals_result,early_stopping_rounds=100)
+model = XGBClassifier(**param)
+model.fit(X=Xtrain,y=Ytrain,eval_set=[(Xtrain,Ytrain),(Xtest,Ytest)],eval_metric='error')
+evals_result = model.evals_result()
 model.save_model(filePath("model.txt"))
 
 evalOutput = open(filePath("evalOutput"),"w+")
 
 
 i=0
-while i < len(evals_result['eval']['error']):
-    evalOutput.write(str(evals_result['train']['error'][i])+"\t"+str(evals_result['eval']['error'][i])+"\n")
+while i < len(evals_result['validation_0']['error']):
+    evalOutput.write(str(evals_result['validation_0']['error'][i])+"\t"+str(evals_result['validation_1']['error'][i])+"\n")
     i+=1
 print("done")
 
-#model = XGB.cv(dtrain=dataTrain, params=param,nfold=10,metrics="auc",seed=1)
-#print(XGB.get_config())
-# print(f'fullnput: {fullInput[6]}')
-# pre = model.predict(DMatrix([fullInput[6]], feature_names=labels))
-# print(fullOutput[6])
-# print(pre)
-XGB.plot_importance(model)
+cmLabels = ["Malicious","Benign"]
+predictions = model.predict(X=fullInput)
+cm = SKL.confusion_matrix(fullOutput, predictions)
+disp = SKL.ConfusionMatrixDisplay(confusion_matrix=cm,display_labels=cmLabels)
+disp.plot()
 MPL.show()
 
-
-
-#SKL.recall_score()
-
-
-
-#fullInput = maliciousInput + benignInput
-#print(fullInput)
 # Setup:
 # 0             1                       2                               3                   4                       5                   6                               7                                     8
 # Duration      Size of flows orig      total size of flows resp        ratio of sizes      outbound packets        inbound packets     length of certificate path      number of domains in certificate      certificate length
